2e6dcd266e0431dc2d1a0d82e6c21518b9031a0a
AleksanMakhyan <alexmakhyan@gmail.com>
2026-09-10 19:32:20 +0000

    Lab 8 inputs complete

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -7,8 +7,30 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your first name?");
+		String firstName = sc.nextLine();
+		System.out.println("How old are you?");
+		int age = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Which month were you born?");
+		String month = sc.nextLine();
+		System.out.println("Which day were you born?");
+		int day = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Which year were you born?");
+		int year = sc.nextInt();
+		sc.nextLine();
+		System.out.println("How much is a buck fifty?");
+		double buckfifty = sc.nextDouble();
+		sc.nextLine();
+
+		System.out.println("Here is the information we found:");
+		System.out.println("First Name: " + firstName);
+		System.out.println("Age: " + age);
+		System.out.println("Birthday Month: " + month);
+		System.out.println("Birthday Day: " + day);
+		System.out.println("Birthday Year: " + year);
+		System.out.println("Buck Fifty Value: " + buckfifty);
 	}
 }
