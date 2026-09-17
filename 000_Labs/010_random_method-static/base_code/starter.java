746c7713cf1dbda46a0efed9737abaf14c0c505b
AleksanMakhyan <alexmakhyan@gmail.com>
2026-09-17 18:58:40 +0000

    update

--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -1,15 +1,12 @@
-/*
- *	Author:  
- *  Date: 
-*/
 
 import java.util.Scanner;
 import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("An Integer range 0-9 " + ((int)(Math.random() * 10)));
+		System.out.println("An Integer range 1-100 "  + ((int)(Math.random() * 100 + 1)));
+		System.out.println("A Double range 2.5-3.5 " + ((double)(Math.random() + 2.5)));
+		System.out.println("A Double range 14-589 " + ((double)(Math.random() * 575 + 14)));
 	}
 }
