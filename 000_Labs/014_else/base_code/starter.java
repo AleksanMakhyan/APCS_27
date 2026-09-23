44fdda83027e7d03f54f065d830a5f48cdccb1ab
AleksanMakhyan <alexmakhyan@gmail.com>
2026-09-23 06:05:39 +0000

    done lab 14

--- 000_Labs/014_else/base_code/starter.java
+++ 000_Labs/014_else/base_code/starter.java
@@ -8,8 +8,15 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+			Scanner sc = new Scanner(System.in);
+	System.out.println("Pick a number between 1 - 1000: ");
+	int number = sc.nextInt();
+	int random = (int)(Math.random() * 1000 + 1);
+	if (random == number) {
+		System.out.println("CONGRATS YOU GUESSED THE NUMBER! THE NUMBER WAS " + random);
+	}
+	if (random != number) {
+		System.out.println("Your guess was wrong, the number was: " + random);
+	}
 	}
 }
