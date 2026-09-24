41fb4f039960893315039705589d7c998b45ade3
AleksanMakhyan <alexmakhyan@gmail.com>
2026-09-24 19:33:47 +0000

    hw4

--- 100_HW/HW4_Guessing_Game/base_code/starter.java
+++ 100_HW/HW4_Guessing_Game/base_code/starter.java
@@ -8,8 +8,44 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+Scanner sc = new Scanner(System.in);
+System.out.println("The goal of the game is to guess a word with two hints! ");
+int randomizer = (int)(Math.random() *2 + 1);
+String answer = "";
+String hint1 = "";
+String hint2 = "";
+if (randomizer == 1) {
+	answer = "dog";
+	hint1 = "It's a common household pet!";
+	hint2 = "They're known for their loyalty and friendly nature!";
+}
+if (randomizer == 2) {
+	answer = "apple";
+	hint1 = "It's a fruit that grows on a tree!";
+	hint2 = "An _____ a day keeps the doctor away!";
+}
+if (randomizer == 3) {	
+	answer = "cloud";
+	hint1 = "When you look up into the sky you can see them!";
+	hint2 = "It's often white or grey depending on the weather!";
+}
+System.out.println(hint1);
+System.out.print("What is your guess? ");
+String response = sc.nextLine();
+
+if (response.equalsIgnoreCase(answer)) {
+	System.out.println("You got it! Woo!!!");
+} else {
+	System.out.println("You sadly didn't guess it right, here's another hint!");
+	System.out.println(hint2);
+	System.out.println("What is your second guess? ");
+	String response2 = sc.nextLine();
+	if (response.equalsIgnoreCase(answer)) {
+		System.out.println("YAY you got it correct!");
+	} else {
+		System.out.println("Sorry you have used up all your hints, please try again! ");
+	}
+}
+	
 	}
 }
